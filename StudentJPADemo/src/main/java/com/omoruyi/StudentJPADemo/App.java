e4fb2e95bd7d6469196f82c685fdfc59021a62d4
Omoruyi Omofonmwan <OmoruyiOmofonmwan@yahoo.com>
2020-02-23 16:13:46 -0600

    Started a new project

---
+++ StudentJPADemo/src/main/java/com/omoruyi/StudentJPADemo/App.java
@@ -0,0 +1,13 @@
+package com.omoruyi.StudentJPADemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
